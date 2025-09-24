1c4dcb48b4f8b6109152847c1b890e65638711fe
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-09-23 17:40:50 -0700

    agregué menú en dispositivos móviles

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  mostrarMenu() {
+    alert('Al hacer clic aquí debería mostrarse el menú');
+  }
+}
